8e2469f9d30062932eb0da93dacba46ecf246af4
vudinhdai <vudinhdai1999hnue@gmail.com>
2019-08-01 11:29:11 +0700

    cm

---
+++ src/Element.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Element {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("enter size:");
+            size = scanner.nextInt();
+            if (size > 20) {
+
+            }
+        } while (size < 20);
+
+        array = new int[size];
+        int i=0;
+        while (i<array.length){
+            System.out.println(" enter element:"+(i+1) +":");
+            array[i]=scanner.nextInt();
+            i++;
+
+        }
+
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+        for (int j = 0; j < array.length / 2; j++) {
+            int temp = array[j];
+            array[j] = array[size - 1 - j];
+            array[size - 1 - j] = temp;
+        }
+
+        System.out.printf("%-20s%s", "Reverse array: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+
+    }
+
+
+}
